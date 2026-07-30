873d4318e08ae3b57ab1eb3971b9f9398b80f8ab
Stanislav <pingo.mw2@gmail.com>
2026-07-30 12:23:23 +0200

    fix: focus on name input

--- src/components/FormatterCore.jsx
+++ src/components/FormatterCore.jsx
@@ -519,7 +519,7 @@ export default function FormatterCore({
       <div className="limit">
         <div className="main-input-number-block">
           <div className="input-name-block">
-            <button type="button" className="button-number button-decrement" onClick={() => changeNumber(-1)}>
+            <button type="button" tabIndex={-1} className="button-number button-decrement" onClick={() => changeNumber(-1)}>
               <svg viewBox="0 0 15 3" xmlns="http://www.w3.org/2000/svg">
                 <path d="M0 1.5C0 0.671573 0.671573 0 1.5 0H13.5C14.3284 0 15 0.671573 15 1.5C15 2.32843 14.3284 3 13.5 3H1.5C0.671573 3 0 2.32843 0 1.5Z" />
               </svg>
@@ -529,6 +529,7 @@ export default function FormatterCore({
               <div className="field__line"></div>
               <input
                 ref={fileNameInputRef}
+                tabIndex={1}
                 className="field__area input-name"
                 id="fileName"
                 type="text"
@@ -539,7 +540,7 @@ export default function FormatterCore({
               />
             </div>
 
-            <button type="button" className="button-number button-increment" onClick={() => changeNumber(1)}>
+            <button type="button" tabIndex={-1} className="button-number button-increment" onClick={() => changeNumber(1)}>
               <svg viewBox="0 0 15 15" xmlns="http://www.w3.org/2000/svg">
                 <path d="M0 7.5C0 6.67157 0.671573 6 1.5 6H13.5C14.3284 6 15 6.67157 15 7.5C15 8.32843 14.3284 9 13.5 9H1.5C0.671573 9 0 8.32843 0 7.5Z" />
                 <path d="M7.5 15C6.67157 15 6 14.3284 6 13.5L6 1.5C6 0.671573 6.67157 0 7.5 0V0C8.32843 0 9 0.671573 9 1.5V13.5C9 14.3284 8.32843 15 7.5 15V15Z" />
@@ -570,6 +571,7 @@ export default function FormatterCore({
                 <div className="field-big__line"></div>
                 <div
                   ref={editorRef}
+                  tabIndex={2}
                   id="editor"
                   className="field-big__area field-big__area_main primary-text-editor-block"
                   contentEditable="true"
